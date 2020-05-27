b09acfa6ef957a2f0be4d62cbd1a743c6e4de53e
stacklast <softwareywebsoluciones@gmail.com>
2020-05-26 22:57:11 -0500

    Ion List en HomePage

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-
+  componentes:Componente[]=[
+    {
+      icon: 'american-footaball-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-store',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+interface Componente {
+    icon:string,
+    name:string,
+    redirectTo:string
 }
