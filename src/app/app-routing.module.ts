e7d1b7d0c067508b0e47aad1a77e7b0c4d2c4485
stacklast <softwareywebsoluciones@gmail.com>
2020-06-13 16:54:09 -0500

    ion-button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
     path: 'avatar',
     loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
   },
+  {
+    path: 'botones',
+    loadChildren: () => import('./pages/botones/botones.module').then( m => m.BotonesPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -23,6 +23,12 @@ export class InicioPage implements OnInit {
       name: 'Avatar',
       redirectTo: '/avatar'
     }
+    ,
+    {
+      icon: 'radio-button-on',
+      name: 'Botones Router',
+      redirectTo: '/botones'
+    }
   ];
   constructor() { }
 
